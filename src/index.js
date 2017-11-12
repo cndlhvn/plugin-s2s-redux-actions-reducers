827a3cca5841949dc48862c38cc33b25b45cb2d6
candle <ted@wicrep.com>
2017-11-12 23:01:02 +0900

    Create main script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
-module.exports = (babel) => {
-  console.log('plugin enabled')
-  return {}
+module.exports = babel => {
+  const t = babel.types;
+
+  return {
+    name: "s2s-redux-actions-reducers",
+    visitor: {
+      ObjectProperty: function(path){
+        if(!path.node.shorthand){
+          return
+        }
+        const ObjectExpression = path.find(parent => parent.isObjectExpression())
+        const actionName = path.node.value.name
+
+        const reducerObjectProperty = t.ObjectProperty(
+          t.MemberExpression(t.identifier("actions"),t.identifier(actionName)),
+          t.ArrowFunctionExpression(
+            [t.Identifier("state"),t.Identifier("action")],
+            t.ObjectExpression(
+              [t.SpreadProperty(t.Identifier("state"))]
+            )
+          ),true
+        )
+
+        path.replaceWith(reducerObjectProperty)
+      }
+    }
+  };
 }
